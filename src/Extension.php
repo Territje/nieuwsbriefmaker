c348d60e33e0fa8baff58bbbf699b30f0608376f
Territje <42745066+Territje@users.noreply.github.com>
2021-02-16 15:39:15 +0100

    Create Extension.php

---
+++ src/Extension.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Terri\Nieuwsbriefmaker;
+
+use Bolt\Extension\BaseExtension;
+
+class Extension extends BaseExtension
+{
+    public function getName(): string
+    {
+        return 'Nieuwsbriefmaker';
+    }
+}
+
+?>
